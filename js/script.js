121f2cc1447368624e05f91d39df1caf81d53b74
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-22 02:16:19 +0200

    Creare ciclo for per far apparire i numeri in pagina e test del timer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log('Js-simon')
+
+
+// scrivere in console 5 numeri
+for(let i=1;i<5+1;i++){
+    const section = document.createElement('section')
+    document.body.append(section)
+    let span = document.createElement('span')
+    section.appendChild(span)
+    span.textContent = Number.parseInt(Math.random()*i,10)
+}
+
+// creazione timer
+setTimeout (myFunction, 30000)
+    function myFunction(){
+        console.log('Timer funzionante')
+        prompt('quali sono i numeri presenti?')
+    }
