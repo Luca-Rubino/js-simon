39ac3dec33a057a17f0e892d06622a380478e302
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-27 22:14:59 +0200

    creare 2 cicli for: il primo per generare 5 numeri casuali interi ed inserirli in un array ed il secondo per creare ed inserire nel dom 5 tag span che conterranno ogniuno uno dei 5 numeri creati

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 console.log('Js-simon')
 
+let i, span, span1
 
-// scrivere in console 5 numeri
-for(let i=1;i<5+1;i++){
-    const section = document.createElement('section')
-    document.body.append(section)
-    let span = document.createElement('span')
-    section.appendChild(span)
-    span.textContent = Number.parseInt(Math.random()*i,10)
+const section = document.createElement('section');
+
+document.body.append(section);
+
+
+let randomNum = [];
+for(i=0; i < 5; i++){
+    randomNum.push(Number.parseInt(Math.random() * 100, 10));
+};
+
+
+for(i=0; i < 5; i++){
+    span = document.createElement('span');
+    section.appendChild(span);
+    span.textContent = randomNum[i]
 }
 
 // creazione timer
-setTimeout (myFunction, 30000)
-    function myFunction(){
-        console.log('Timer funzionante')
-        prompt('quali sono i numeri presenti?')
-    }
+setTimeout (myFunction, 30000);
+
+function myFunction(){
+    console.log('Timer funzionante')
+    prompt('quali sono i numeri presenti?')
+}
