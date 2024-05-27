83d8c5b64aefd76e7310001db52858581db2f371
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-27 22:58:10 +0200

    Creare la funzione per inserire tramite il prompt un numero e se è uguale al primo numero generato restituirà in console il risultato true

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 console.log('Js-simon')
 
-let i, span, span1
+let i, span, num1, num2, num3, num4, num5
 
 const section = document.createElement('section');
 
@@ -16,13 +16,21 @@ for(i=0; i < 5; i++){
 for(i=0; i < 5; i++){
     span = document.createElement('span');
     section.appendChild(span);
-    span.textContent = randomNum[i]
+    span.textContent = randomNum[i];
 }
 
 // creazione timer
-setTimeout (myFunction, 30000);
+setTimeout (quizNumeri, 30000);
 
-function myFunction(){
-    console.log('Timer funzionante')
-    prompt('quali sono i numeri presenti?')
-}
+function quizNumeri(){
+    console.log('Timer funzionante');
+    num1 = prompt(`scrivi il primo numero`);
+    num1 !== randomNum[0];
+    if(num1 == randomNum[0]){
+        num1 = true;
+        console.log(num1);
+    } else {
+        num1 = false;
+        console.log(num1);
+    };
+};
